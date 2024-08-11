8af460e21524244a6e966f2bb83e886b3143c375
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-08-11 19:11:45 -0300

    Initial Commit

---
+++ src/main/java/com/smartshop/smartshop_backend/SmartshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartshop.smartshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartshopBackendApplication.class, args);
+	}
+
+}
